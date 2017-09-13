85221583b5cfc1660870527d873deffaf9f39fd4
WooodHead <WooodHead@users.noreply.github.com>
2017-09-13 18:32:05 +0800

    use gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,171 +1,67 @@
-var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  rename = require('gulp-rename'),
-  cleanCSS = require('gulp-clean-css'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-  jshint = require('gulp-jshint'),
-  prefix = require('gulp-autoprefixer'),
-  browserSync = require('browser-sync'),
-  reload = browserSync.reload,
-  htmlmin = require('gulp-htmlmin'),
-  size = require('gulp-size'),
-  imagemin = require('gulp-imagemin'),
-  pngquant = require('imagemin-pngquant'),
-  plumber = require('gulp-plumber'),
-  deploy = require('gulp-gh-pages'),
-  notify = require('gulp-notify'),
-  sassLint = require('gulp-sass-lint'),
-  del = require('del'),
-  vinylPaths = require('vinyl-paths'),
-  sourcemaps = require('gulp-sourcemaps'),
-  colors = require('colors'),
-  sassdoc = require('sassdoc'),
-  // Temporary solution until gulp 4
-  // https://github.com/gulpjs/gulp/issues/355
-  runSequence = require('run-sequence');
+// gulpfile.js
+const gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    htmlmin = require('gulp-htmlmin'),
+    nunjucksRender = require('gulp-nunjucks-render'); // importing the plugin
 
-var gulp = require('gulp'),
-  livereload = require('gulp-livereload');
-
-
-var bases = {
-  pages: ['home', 'login', 'speak'],
-  src: 'src',
-  home: 'src/home/',
-  login: 'src/login/',
-  speak: 'src/speak/',
-
-  dist: 'dist/',
-};
-
-colors.setTheme({
-  silly: 'rainbow',
-  input: 'grey',
-  verbose: 'cyan',
-  prompt: 'grey',
-  info: 'green',
-  data: 'grey',
-  help: 'cyan',
-  warn: 'yellow',
-  debug: 'blue',
-  error: 'red'
-});
-
-var displayError = function (error) {
-  // Initial building up of the error
-  var errorString = '[' + error.plugin.error.bold + ']';
-  errorString += ' ' + error.message.replace("\n", ''); // Removes new line at the end
-
-  // If the error contains the filename or line number add it to the string
-  if (error.fileName)
-    errorString += ' in ' + error.fileName;
-
-  if (error.lineNumber)
-    errorString += ' on line ' + error.lineNumber.bold;
-
-  // This will output an error like the following:
-  // [gulp-sass] error message in file_name on line 1
-  console.error(errorString);
+const PATHS = {
+    output: 'dist',
+    templates: 'src/templates',
+    pages: 'src/pages',
 }
 
-var onError = function (err) {
-  notify.onError({
-    title: "Gulp",
-    subtitle: "Failure!",
-    message: "Error: <%= error.message %>",
-    sound: "Basso"
-  })(err);
-  this.emit('end');
-};
+// writing up the gulp nunjucks task
+gulp.task('nunjucks', function () {
+    console.log('Rendering nunjucks files..');
+    return gulp.src(PATHS.pages + '/**/*.+(html|js|css)')
+        .pipe(nunjucksRender({
+            path: [PATHS.templates],
+            watch: true,
+        }))
+        .pipe(gulp.dest(PATHS.output));
+});
 
-var sassOptions = {
-  outputStyle: 'expanded'
-};
-
-var prefixerOptions = {
-  browsers: ['last 2 versions']
-};
-
-// BUILD SUBTASKS
-// ---------------
-
-gulp.task('clean:dist', function () {
-  return gulp.src(bases.dist)
-    .pipe(vinylPaths(del));
+gulp.task('helpPage', function () {
+    console.log('Rendering nunjucks files..');
+    return gulp.src(PATHS.pages + '/help/help/**/*.+(html|js|css)')
+        .pipe(nunjucksRender({
+            path: [PATHS.template],
+            watch: true,
+        }))
+        .pipe(gulp.dest(PATHS.output));
 });
 
 
-
-
-gulp.task('lib', function () {
-  // copy modernizr to dist directly
-  gulp.src('bower_components/**/*.*')
-    .pipe(gulp.dest(bases.dist + '/bower_components'));
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: PATHS.output
+        },
+    });
 });
-
-
-
-gulp.task('html', function () {
-  bases.pages.forEach(function (item, index) {
-    gulp.src(bases.src + '/' + item + '/*.html')
-      .pipe(htmlmin({
-        collapseWhitespace: true
-      }))
-      .pipe(gulp.dest(bases.dist + '/' + item))
-      .pipe(reload({
-        stream: true
-      }))
-      .pipe(livereload());
-  });
-
-});
-
-gulp.task('css', function () {
-  bases.pages.forEach(function (item, index) {
-    gulp.src(bases.src + '/' + item + '/*.css')
-      .pipe(gulp.dest(bases.dist + '/' + item))
-      .pipe(livereload());
-  });
-});
-
-
-gulp.task('js', function () {
-  bases.pages.forEach(function (item, index) {
-    gulp.src(bases.src + '/' + item + '/*.js')
-      .pipe(gulp.dest(bases.dist + '/' + item))
-      .pipe(livereload());
-  });
-});
-
-gulp.task('img', function () {
-  // copy modernizr to dist directly
-  bases.pages.forEach(function (item, index) {
-    gulp.src(bases.src + '/' + item + '/img/**/*.*')
-      .pipe(gulp.dest(bases.dist + '/' + item + '/img'))
-      .pipe(livereload());
-  });
-});
-
-
 
 gulp.task('watch', function () {
-  livereload.listen();
-  gulp.watch(bases.src + '/**/*.html', ['html']);
-  gulp.watch(bases.src + '/**/*.css', ['css']);
-  gulp.watch(bases.src + '/**/img/*', ['img']);
-  gulp.watch(bases.src + '/**/*.js', ['js']);
+    // trigger Nunjucks render when pages or templates changes
+    gulp.watch([PATHS.pages + '/**/*.+(html|js|css)', PATHS.templates + '/**/*.+(html|js|css)'], ['nunjucks'])
 
+    // reload browsersync when `dist` changes
+    gulp.watch(PATHS.output + '/*').on('change', browserSync.reload);
 });
 
-// BUILD TASKS
-// ------------
-
-
-gulp.task('default', function (done) {
-  runSequence('build', done);
+gulp.task('minify', function () {
+    return gulp.src(PATHS.output + '/*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            cssmin: true,
+            jsmin: true,
+            removeOptionalTags: true,
+            removeComments: false
+        }))
+        .pipe(gulp.dest(PATHS.output));
 });
 
-gulp.task('build', function (done) {
-  runSequence('clean:dist', 'html', 'css', 'js', 'img', 'lib', done);
-});
+// run browserSync auto-reload together with nunjucks auto-render
+gulp.task('auto', ['browserSync', 'watch']);
+
+//default task to be run with gulp
+gulp.task('default', ['nunjucks']);
---
+++ old/gulpfile.js
@@ -0,0 +1,171 @@
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  rename = require('gulp-rename'),
+  cleanCSS = require('gulp-clean-css'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  jshint = require('gulp-jshint'),
+  prefix = require('gulp-autoprefixer'),
+  browserSync = require('browser-sync'),
+  reload = browserSync.reload,
+  htmlmin = require('gulp-htmlmin'),
+  size = require('gulp-size'),
+  imagemin = require('gulp-imagemin'),
+  pngquant = require('imagemin-pngquant'),
+  plumber = require('gulp-plumber'),
+  deploy = require('gulp-gh-pages'),
+  notify = require('gulp-notify'),
+  sassLint = require('gulp-sass-lint'),
+  del = require('del'),
+  vinylPaths = require('vinyl-paths'),
+  sourcemaps = require('gulp-sourcemaps'),
+  colors = require('colors'),
+  sassdoc = require('sassdoc'),
+  // Temporary solution until gulp 4
+  // https://github.com/gulpjs/gulp/issues/355
+  runSequence = require('run-sequence');
+
+var gulp = require('gulp'),
+  livereload = require('gulp-livereload');
+
+
+var bases = {
+  pages: ['home', 'login', 'speak'],
+  src: 'src',
+  home: 'src/home/',
+  login: 'src/login/',
+  speak: 'src/speak/',
+
+  dist: 'dist/',
+};
+
+colors.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
+
+var displayError = function (error) {
+  // Initial building up of the error
+  var errorString = '[' + error.plugin.error.bold + ']';
+  errorString += ' ' + error.message.replace("\n", ''); // Removes new line at the end
+
+  // If the error contains the filename or line number add it to the string
+  if (error.fileName)
+    errorString += ' in ' + error.fileName;
+
+  if (error.lineNumber)
+    errorString += ' on line ' + error.lineNumber.bold;
+
+  // This will output an error like the following:
+  // [gulp-sass] error message in file_name on line 1
+  console.error(errorString);
+}
+
+var onError = function (err) {
+  notify.onError({
+    title: "Gulp",
+    subtitle: "Failure!",
+    message: "Error: <%= error.message %>",
+    sound: "Basso"
+  })(err);
+  this.emit('end');
+};
+
+var sassOptions = {
+  outputStyle: 'expanded'
+};
+
+var prefixerOptions = {
+  browsers: ['last 2 versions']
+};
+
+// BUILD SUBTASKS
+// ---------------
+
+gulp.task('clean:dist', function () {
+  return gulp.src(bases.dist)
+    .pipe(vinylPaths(del));
+});
+
+
+
+
+gulp.task('lib', function () {
+  // copy modernizr to dist directly
+  gulp.src('bower_components/**/*.*')
+    .pipe(gulp.dest(bases.dist + '/bower_components'));
+});
+
+
+
+gulp.task('html', function () {
+  bases.pages.forEach(function (item, index) {
+    gulp.src(bases.src + '/' + item + '/*.html')
+      .pipe(htmlmin({
+        collapseWhitespace: true
+      }))
+      .pipe(gulp.dest(bases.dist + '/' + item))
+      .pipe(reload({
+        stream: true
+      }))
+      .pipe(livereload());
+  });
+
+});
+
+gulp.task('css', function () {
+  bases.pages.forEach(function (item, index) {
+    gulp.src(bases.src + '/' + item + '/*.css')
+      .pipe(gulp.dest(bases.dist + '/' + item))
+      .pipe(livereload());
+  });
+});
+
+
+gulp.task('js', function () {
+  bases.pages.forEach(function (item, index) {
+    gulp.src(bases.src + '/' + item + '/*.js')
+      .pipe(gulp.dest(bases.dist + '/' + item))
+      .pipe(livereload());
+  });
+});
+
+gulp.task('img', function () {
+  // copy modernizr to dist directly
+  bases.pages.forEach(function (item, index) {
+    gulp.src(bases.src + '/' + item + '/img/**/*.*')
+      .pipe(gulp.dest(bases.dist + '/' + item + '/img'))
+      .pipe(livereload());
+  });
+});
+
+
+
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch(bases.src + '/**/*.html', ['html']);
+  gulp.watch(bases.src + '/**/*.css', ['css']);
+  gulp.watch(bases.src + '/**/img/*', ['img']);
+  gulp.watch(bases.src + '/**/*.js', ['js']);
+
+});
+
+// BUILD TASKS
+// ------------
+
+
+gulp.task('default', function (done) {
+  runSequence('build', done);
+});
+
+gulp.task('build', function (done) {
+  runSequence('clean:dist', 'html', 'css', 'js', 'img', 'lib', done);
+});
